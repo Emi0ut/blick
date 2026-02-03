93fde0cabc18f3e964c199ff848a76bee8ae2a7c
abhimanyu <abhimanyutarar187@gmail.com>
2026-02-03 15:11:31 +0530

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class operator {
+    public static void main(String[] arg){
+    Scanner sc=new Scanner(System.in);
+    float x= sc.nextFloat();
+    if (x%2==0){
+        System.out.println("even");
+    }else{
+        System.out.println("Odd");
+    }
+
+    
+}
+}
